8d868bcea2a0c2c77c1da852e3ee4e96f2cc2b34
Valentin Meier <valentinmeier123@gmail.com>
2022-04-28 02:34:53 -0300

    toman.es

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+menu = document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e) => {
+    
+menu.classList.toggle('active');
+});
